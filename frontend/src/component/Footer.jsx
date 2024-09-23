a48c9ef584ba0995a434c59b0337128c0f908a1b
pivnati <natanmuleta911@gmail.com>
2024-09-23 14:51:53 -0700

    create the footer.jsx  inside the components

---
+++ frontend/src/component/Footer.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-green-900 p-4 text-white text-center">
+      <p>&copy; 2024 Milk Uber. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
