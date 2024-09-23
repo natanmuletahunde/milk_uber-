7b20942032e1d232b5c60f46ff42184a20aa441c
pivnati <natanmuleta911@gmail.com>
2024-09-23 14:49:58 -0700

    create the Main.components

---
+++ frontend/src/component/Main.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Services from './Services';
+
+const Main = () => {
+  return (
+    <main className="my-8">
+      <h2 className="text-3xl font-bold text-center mb-6">Deliver Fresh Milk to Your Doorstep</h2>
+      <p className="text-center mb-6">Order milk online and get it delivered quickly by Uber drivers.</p>
+      <Services />
+    </main>
+  );
+};
+
+export default Main;
