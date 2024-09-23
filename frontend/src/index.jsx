a725e404947d23994549c0bae782ab29100e7ae7
pivnati <natanmuleta911@gmail.com>
2024-09-23 14:57:53 -0700

    modify the main.jsx

---
+++ frontend/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import About from './About';
+import Contact from './Contact';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+);
